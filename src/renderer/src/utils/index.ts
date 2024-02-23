c545e9fb51e5209a4ca0775481e52af0d7294484
Mornieur <mariafernanda-coro@hotmail.com>
2024-02-23 11:15:20 -0300

    feat: create a date format function

---
+++ src/renderer/src/utils/index.ts
@@ -0,0 +1,16 @@
+import clsx, { ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+const locale = (window.context && window.context.locale) || 'en-US'
+
+const dateFormatter = new Intl.DateTimeFormat(locale, {
+  dateStyle: 'short',
+  timeStyle: 'short',
+  timeZone: 'UTC'
+})
+
+export const formatDateFromMs = (ms: number) => dateFormatter.format(ms)
+
+export const cn = (...args: ClassValue[]) => {
+  return twMerge(clsx(...args))
+}
