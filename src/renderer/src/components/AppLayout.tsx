240c9237dcf361a964b1dee7b7fa5ecef511fbe8
Mornieur <mariafernanda-coro@hotmail.com>
2024-03-12 11:53:01 -0300

    feat: add RootLayout, Sidebar, and Content components

--- src/renderer/src/components/AppLayout.tsx
+++ src/renderer/src/components/AppLayout.tsx
@@ -1,7 +1,7 @@
 import { ComponentProps, forwardRef } from 'react'
 import { twMerge } from 'tailwind-merge'
 
-export const RootLayout = ({ className, children, ...props }: ComponentProps<'main'>) => {
+export const RootLayout = ({ children, className, ...props }: ComponentProps<'main'>) => {
   return (
     <main className={twMerge('flex flex-row h-screen', className)} {...props}>
       {children}
