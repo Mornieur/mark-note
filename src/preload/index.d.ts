b3f4b8939ff322c8aaef2bdc38f204df4c524af3
Mornieur <mariafernanda-coro@hotmail.com>
2024-03-12 11:44:40 -0300

    feat: add function into the context

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,10 +1,15 @@
-import { ElectronAPI } from '@electron-toolkit/preload'
+import { CreateNote, DeleteNote, GetNotes, ReadNote, WriteNote } from '@shared/types'
 
 declare global {
   interface Window {
-    electron: ElectronAPI
+    // electron: ElectronAPI
     context: {
       locale: string
+      getNotes: GetNotes
+      readNote: ReadNote
+      writeNote: WriteNote
+      createNote: CreateNote
+      deleteNote: DeleteNote
     }
   }
 }
